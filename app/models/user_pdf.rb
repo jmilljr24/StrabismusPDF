bab21702078f9b9dee5388d4c363d40ac5c9ad01
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-07 12:29:56 -0500

    Add file type validation for pdf and size

--- app/models/user_pdf.rb
+++ app/models/user_pdf.rb
@@ -1,3 +1,16 @@
 class UserPdf < ApplicationRecord
   has_one_attached :pdf, dependent: :destroy
+  validate :acceptable_pdf
+
+  private
+
+  def acceptable_pdf
+    return unless pdf.attached?
+
+    errors.add(:pdf, 'is too big. Max 10MB') unless pdf.blob.byte_size <= 10.megabyte
+
+    return if pdf.content_type.in?(%w[application/pdf])
+
+    errors.add(:pdf, 'must be a PDF')
+  end
 end
