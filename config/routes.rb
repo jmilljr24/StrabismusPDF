7e2800526c2e39951cac1138451db3beba2bda7c
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-08 10:45:48 -0500

    Add parse controller for no db processing of file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :user_pdfs
+  # resources :parse
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-
+  post 'parse' => 'parse#parse_file', as: :parse_file
+  get 'parse/show' => 'parse#show', as: :parse_show
+  get 'downloads' => 'parse#download', as: :download
   # Defines the root path route ("/")
-  root 'user_pdfs#index'
+  # root 'user_pdfs#index'
+  root 'parse#index'
 end
