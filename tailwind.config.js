60d7d1091e96a193d9211ecbd8edfbaf248a7f7a
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-09 06:45:05 -0500

    Add styling. Change pdf download to open in new tab

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,5 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/assets/stylesheets/**/*.css",
     "./app/javascript/**/*.js",
-    "./node_modules/flowbite/**/*.js",
   ],
-  plugins: [require("flowbite/plugin")],
 };
