6aee35f66c2499ffaae772d0a89c4d29fc7e0522
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-13 17:49:41 -0500

    Remove dropzone from js controller index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import DropzoneController from "./dropzone_controller"
-application.register("dropzone", DropzoneController)
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
