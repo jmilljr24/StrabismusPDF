ba790fb3c01805183af7703074e439379a4b6764
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-12 20:03:59 -0500

    Change file size upload to 70mb

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import DropzoneController from "./dropzone_controller"
+application.register("dropzone", DropzoneController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
