3ae76ceeea8d58b73927588ec9ab50522ce94190
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-14 17:58:24 -0500

    Add dropzone from deanin
    Not finished. Need to connect upload file to location of parse

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
+import DropzoneController from "./dropzone_controller"
+application.register("dropzone", DropzoneController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
