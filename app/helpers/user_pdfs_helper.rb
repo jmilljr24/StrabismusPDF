50ddc7507d08ce224d66110b6eb100052224c8b6
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-15 14:21:57 -0500

    Creater user_pdf helper for pdf colorizer using user_pdf id

--- app/helpers/user_pdfs_helper.rb
+++ app/helpers/user_pdfs_helper.rb
@@ -1,2 +1,31 @@
 module UserPdfsHelper
+  def colorizer(user_pdf) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+    file = user_pdf.pdf.blob
+    file.open do |tempfile|
+      doc = HexaPDF::Document.open(tempfile.path)
+      doc.pages.each_with_index do |page, index|
+        puts "Processing page #{index + 1}"
+        processor = if index == 0
+                      StringBoxesProcessor.new(page)
+                    else
+                      StringBoxesProcessor.new(page, @color_key)
+                    end
+        page.process_contents(processor)
+        str_boxes = processor.str_boxes
+        processor.match(str_boxes)
+        page_parts = processor.page_parts
+
+        both_pages = @prev_page_parts & processor.current_page_parts.uniq
+
+        processor.assign_color(page_parts, both_pages)
+        processor.color(page_parts)
+        @color_key = processor.color_key
+        @prev_page_parts = processor.current_page_parts.uniq
+      end
+      @filename = Rails.root.join('tmp', 'Plans_Highlight.pdf').to_s
+      doc.write(@filename, optimize: true)
+      file = File.open(@filename)
+      @blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: 'Highlights.pdf')
+    end
+  end
 end
