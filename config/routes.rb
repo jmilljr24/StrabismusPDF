926affd0c57517f3abc3e013f1561cfd0b7193e0
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-15 16:17:23 -0500

    Add information page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'parse/show' => 'parse#show', as: :parse_show
   get 'downloads' => 'parse#download', as: :download
   get 'preview' => 'parse#preview', as: :preview
-
+  get 'information' => 'application#information'
   # Defines the root path route ("/")
   root 'user_pdfs#new'
   # root 'parse#index'
