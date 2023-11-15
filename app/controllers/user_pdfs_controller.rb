18d10e2c56ff462a1f0e3f4b0153092034238e51
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-15 14:21:57 -0500

    Creater user_pdf helper for pdf colorizer using user_pdf id

--- app/controllers/user_pdfs_controller.rb
+++ app/controllers/user_pdfs_controller.rb
@@ -1,6 +1,7 @@
 # require_relative '.../app/string_boxes_processor'
 
 class UserPdfsController < ApplicationController
+  include UserPdfsHelper
   before_action :set_user_pdf, only: %i[show edit update destroy]
 
   # GET /user_pdfs or /user_pdfs.json
@@ -12,34 +13,35 @@ class UserPdfsController < ApplicationController
   end
 
   # GET /user_pdfs/1 or /user_pdfs/1.json
-  def show # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-    file = ActiveStorage::Blob.last
-    file.open do |tempfile|
-      doc = HexaPDF::Document.open(tempfile.path)
-      doc.pages.each_with_index do |page, index|
-        puts "Processing page #{index + 1}"
-        processor = if index == 0
-                      StringBoxesProcessor.new(page)
-                    else
-                      StringBoxesProcessor.new(page, @color_key)
-                    end
-        page.process_contents(processor)
-        str_boxes = processor.str_boxes
-        processor.match(str_boxes)
-        page_parts = processor.page_parts
+  def show
+    colorizer(@user_pdf)
+    # file = ActiveStorage::Blob.last
+    # file.open do |tempfile|
+    #   doc = HexaPDF::Document.open(tempfile.path)
+    #   doc.pages.each_with_index do |page, index|
+    #     puts "Processing page #{index + 1}"
+    #     processor = if index == 0
+    #                   StringBoxesProcessor.new(page)
+    #                 else
+    #                   StringBoxesProcessor.new(page, @color_key)
+    #                 end
+    #     page.process_contents(processor)
+    #     str_boxes = processor.str_boxes
+    #     processor.match(str_boxes)
+    #     page_parts = processor.page_parts
 
-        both_pages = @prev_page_parts & processor.current_page_parts.uniq
+    #     both_pages = @prev_page_parts & processor.current_page_parts.uniq
 
-        processor.assign_color(page_parts, both_pages)
-        processor.color(page_parts)
-        @color_key = processor.color_key
-        @prev_page_parts = processor.current_page_parts.uniq
-      end
-      @filename = Rails.root.join('tmp', 'Plans_Highlight.pdf').to_s
-      doc.write(@filename, optimize: true)
-      file = File.open(@filename)
-      @blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: 'Highlights.pdf')
-    end
+    #     processor.assign_color(page_parts, both_pages)
+    #     processor.color(page_parts)
+    #     @color_key = processor.color_key
+    #     @prev_page_parts = processor.current_page_parts.uniq
+    #   end
+    #   @filename = Rails.root.join('tmp', 'Plans_Highlight.pdf').to_s
+    #   doc.write(@filename, optimize: true)
+    #   file = File.open(@filename)
+    #   @blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: 'Highlights.pdf')
+    # end
   end
 
   # GET /user_pdfs/new
@@ -56,7 +58,7 @@ class UserPdfsController < ApplicationController
 
     respond_to do |format|
       if @user_pdf.save
-        format.html { redirect_to user_pdf_url(@user_pdf), notice: 'User pdf was successfully created.' }
+        format.html { redirect_to user_pdf_url(@user_pdf) }
         format.json { render :show, status: :created, location: @user_pdf }
       else
         format.html { render :new, status: :unprocessable_entity }
