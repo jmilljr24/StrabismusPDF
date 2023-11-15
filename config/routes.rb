1a71d05cd083ef1e34c720566ae38869f9d07e26
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-15 17:52:29 -0500

    Add recolorize link, opens new tab

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'downloads' => 'parse#download', as: :download
   get 'preview' => 'parse#preview', as: :preview
   get 'information' => 'application#information'
+  get 'recolorize' => 'user_pdfs#recolorize', as: :recolorize
   # Defines the root path route ("/")
   root 'user_pdfs#new'
   # root 'parse#index'
