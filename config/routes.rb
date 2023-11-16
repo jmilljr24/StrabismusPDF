dfb5830bc05d31d8d2ac89ff3211f9735bd3c0f4
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-11-16 12:54:48 -0500

    Add turbo download links

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'downloads' => 'parse#download', as: :download
   get 'preview' => 'parse#preview', as: :preview
   get 'information' => 'application#information'
-  get 'user_pdfs/:id/color' => 'user_pdfs#recolorize', as: :recolorize
+  get 'user_pdfs/color/:id' => 'user_pdfs#recolorize', as: :recolorize
   # Defines the root path route ("/")
   # root 'user_pdfs#new'
   root 'user_pdfs#index'
