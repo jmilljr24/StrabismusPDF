f17da87cd3933e30871ef9a2b525a5fe52e40aa7
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-12-03 14:32:58 -0500

    Upload python script to no parts list parsing

--- lib/assets/python/highlight.py
+++ lib/assets/python/highlight.py
@@ -3,6 +3,7 @@ import re
 from io import BytesIO
 import random
 import argparse
+import math
 
 #Color list for highlights
 cl = ['deeppink', 'pink3', 'magenta', 'darkorchid2', 'maroon',
@@ -15,9 +16,9 @@ cl = ['deeppink', 'pink3', 'magenta', 'darkorchid2', 'maroon',
       'snow4']
 
 
-# input_file = "pdfs/27_10.pdf"
+# input_file = "pdfs/09_10.pdf"
 # output_file = "test.pdf"
-parts_re = re.compile(r"[A-Z]{1,2}-\d{3,5}[A-Z]?(-[LR])?")  # need to exclude "(" at beginning tried ^[^\(] but did not work on 07_10. 06_10 was was successfully
+parts_re = re.compile(r"[A-Z]{1,2}-\d{3,5}[A-Z]?(-[L|R])?")  # need to exclude "(" at beginning tried ^[^\(] but did not work on 07_10. 06_10 was was successfully
 #Command line arguments
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-i", "--input", help="Input file path")
@@ -56,6 +57,25 @@ def highlight_left(left_parts):
 def highlight_right(right_parts):
     lr_highlight(right_parts, 'chartreuse4')
 
+def uniq_page_parts(matches):
+    clean_parts = set()
+    left_right_parts = set()
+    for val in matches:
+        #remove unnecessary beginning and trailing characters
+        string = val[4].strip("(,")
+        part =  re.findall(r"[A-Z]{1,2}-\d{3,5}[A-Z]?", string)
+        clean_parts.add(part[0])
+        #find left/right parts
+        lr = re.search(r"-[R|L]", string)
+        if lr:
+            left_right_parts.add(lr.string)
+    return [clean_parts, left_right_parts]
+
+
+
+def last_letter(word):
+    return word[::-1]
+
 #Setup for pdf scan
 pdfDoc = fitz.open(input_file)
 #Print page count of input PDF
@@ -67,9 +87,10 @@ count = 0
 output_buffer = BytesIO()
 
 for pg in range(pdfDoc.page_count):
+    print('Page: ' + str(pg + 1))
     # Select the page
     page = pdfDoc[pg]
-    print( "Processing page " + str(pg + 1))
+
     page_lines = page.get_text("text").split('\n')
     for line in page_lines:
         left_result = re.findall("-L", line)
@@ -82,7 +103,7 @@ for pg in range(pdfDoc.page_count):
     #Process page for parts
     words = page.get_text("words")
     matches = [w for w in words if parts_re.findall(w[4])]
-
+    uniq = uniq_page_parts(matches)
     # for m in matches:
     #     print(m[4])
     
@@ -90,15 +111,24 @@ for pg in range(pdfDoc.page_count):
 
     color_dict = {}
     uniq_parts = 0
-    for val in matches:
-        part_num = val[4]
+    base_set = uniq[0]
+    s = sorted(base_set)
+    contains_highlight = set()
+    for part_num in reversed(s):
+  
         uniq_parts += colorize(part_num)
         matching_val_area = page.search_for(part_num, quads=True)
 
-        highlight = None
-        highlight = page.add_highlight_annot(matching_val_area)
-        highlight.set_colors(stroke= fitz.utils.getColor(color_dict[part_num]))
-        highlight.update(opacity= 0.3)
+        for quad in range(len(matching_val_area)):
+            llx = math.trunc(matching_val_area[quad].ll[0])
+            lly = math.trunc(matching_val_area[quad].ll[1])
+        point = frozenset([llx, lly])
+        if point not in contains_highlight:
+            highlight = None
+            highlight = page.add_highlight_annot(matching_val_area)
+            highlight.set_colors(stroke= fitz.utils.getColor(color_dict[part_num]))
+            highlight.update(opacity= 0.5)
+            contains_highlight.add(point)
 
 print(str(count) + ' Matches found')
 # Save to output
