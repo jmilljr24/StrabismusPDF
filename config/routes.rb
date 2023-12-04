c3577c4ec3f85ba27a102adbdb72c5121565d312
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-12-04 08:33:38 -0500

    Update propfile for production with sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,10 @@ Rails.application.routes.draw do
   root "user_pdfs#index"
 
   # mount Sidekiq::Web in your Rails app
-  mount Sidekiq::Web => "/sidekiq"
+  # mount Sidekiq::Web => "/sidekiq"
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+  mount Sidekiq::Web, at: "/sidekiq"
 end
