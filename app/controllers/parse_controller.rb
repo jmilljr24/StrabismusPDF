ada4f71c2995c788ca22a1feaecda23191b76361
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2023-12-06 19:29:26 -0500

    Comment out hexapdf in parse_controller.

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -19,29 +19,29 @@ class ParseController < ApplicationController
       return
     end
 
-    File.binwrite(Rails.root.join("public", uploaded_file.original_filename), uploaded_file.read)
-    doc = HexaPDF::Document.open(uploaded_file.path)
-    doc.pages.each_with_index do |page, index|
-      puts "Processing page #{index + 1}"
-      processor = if index == 0
-        StringBoxesProcessor.new(page)
-      else
-        StringBoxesProcessor.new(page, @color_key)
-      end
-      page.process_contents(processor)
-      str_boxes = processor.str_boxes
-      processor.match(str_boxes)
-      page_parts = processor.page_parts
+    # File.binwrite(Rails.root.join("public", uploaded_file.original_filename), uploaded_file.read)
+    # doc = HexaPDF::Document.open(uploaded_file.path)
+    # doc.pages.each_with_index do |page, index|
+    #   puts "Processing page #{index + 1}"
+    #   processor = if index == 0
+    #     StringBoxesProcessor.new(page)
+    #   else
+    #     StringBoxesProcessor.new(page, @color_key)
+    #   end
+    #   page.process_contents(processor)
+    #   str_boxes = processor.str_boxes
+    #   processor.match(str_boxes)
+    #   page_parts = processor.page_parts
 
-      both_pages = @prev_page_parts & processor.current_page_parts.uniq
+    #   both_pages = @prev_page_parts & processor.current_page_parts.uniq
 
-      processor.assign_color(page_parts, both_pages)
-      processor.color(page_parts)
-      @color_key = processor.color_key
-      @prev_page_parts = processor.current_page_parts.uniq
-    end
-    @new_file_name = "colored_#{uploaded_file.original_filename}"
-    doc.write("#{Rails.root}/public/#{@new_file_name}", optimize: true)
+    #   processor.assign_color(page_parts, both_pages)
+    #   processor.color(page_parts)
+    #   @color_key = processor.color_key
+    #   @prev_page_parts = processor.current_page_parts.uniq
+    # end
+    # @new_file_name = "colored_#{uploaded_file.original_filename}"
+    # doc.write("#{Rails.root}/public/#{@new_file_name}", optimize: true)
 
     redirect_to parse_show_path(new_file_name: @new_file_name)
   end
