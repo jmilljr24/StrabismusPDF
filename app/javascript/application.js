e7a80b531c6c18140482d1f48164c30d50e889d5
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2024-01-05 08:41:05 -0500

    Install action text with yarn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+import "trix";
+import "@rails/actiontext";
 
 import "trix"
 import "@rails/actiontext"
