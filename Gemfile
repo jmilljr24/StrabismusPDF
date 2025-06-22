b04de826b7f28fb6c186b0249a977842566db8ab
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-06-22 10:30:35 -0400

    add solid / fix tailwind
    more

--- Gemfile
+++ Gemfile
@@ -100,3 +100,13 @@ gem "glib2"
 gem "aws-sdk-s3", "~> 1.142", require: false
 
 gem "devise", "~> 4.9"
+
+gem "solid_queue", "~> 1.1"
+
+gem "solid_cable", "~> 3.0"
+
+gem "foreman", "~> 0.88.1"
+
+gem "tailwindcss-rails", "~> 4.2"
+
+gem "tailwindcss-ruby", "~> 4.1"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,13 +20,16 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = {"cache-control" => "public, max-age=#{2.days.to_i}"}
   else
     config.action_controller.perform_caching = false
   end
 
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
+
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = {database: {writing: :queue}}
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
@@ -38,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
