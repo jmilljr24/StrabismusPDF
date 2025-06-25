357c3530c7ff0cfe607036b0b7d248c6c121964f
jmilljr24 <16829344+jmilljr24@users.noreply.github.com>
2025-06-25 11:49:43 -0400

    loading

--- app/javascript/controllers/upload_controller.js
+++ app/javascript/controllers/upload_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="upload"
 export default class extends Controller {
-  static targets = ["input", "submit", "filename"];
+  static targets = ["input", "submit", "filename", "uploadArea", "loading"];
 
   connect() {
     console.log("upload connected")
     this.toggleSubmit(false);
+    this.hideLoading();
   }
 
   updateFile() {
@@ -19,8 +20,19 @@ export default class extends Controller {
       this.toggleSubmit(false);
     }
   }
+  submit(event) {
+    if (this.submitTarget.disabled) return
 
+    this.submitTarget.disabled = true
+    this.uploadAreaTarget.classList.add("hidden")
+    this.loadingTarget.classList.remove("hidden")
+  }
   toggleSubmit(enabled) {
     this.submitTarget.disabled = !enabled;
+  }
+  hideLoading() {
+    if (this.hasLoadingTarget) {
+      this.loadingTarget.classList.add("hidden")
+    }
   }
 }
